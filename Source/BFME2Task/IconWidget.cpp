e391f93bb3a22e3e692ad974581c6b50c7a87bbb
Jakub Kasprzak <jakub.kasprzak@medapp.pl>
2021-01-05 21:12:55 +0100

    Icon widget added

---
+++ Source/BFME2Task/IconWidget.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "IconWidget.h"
+
+bool UIconWidget::Initialize()
+{
+	return true;
+}
--- Source/BFME2Task/LumberMill.cpp
+++ Source/BFME2Task/LumberMill.cpp
@@ -2,22 +2,31 @@
 
 
 #include "LumberMill.h"
+#include "IconWidget.h"
 
 #include <iostream>
+
 
 // Sets default values
 ALumberMill::ALumberMill()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	IconWidget = NewObject<UIconWidget>();
+	UE_LOG(LogTemp, Warning, TEXT("constructor") );
+	IconWidget->Initialize();
+	if (IconWidget != nullptr)
+		UE_LOG(LogTemp, Warning, TEXT("IconWidget is not null - constructor") );
 }
 
 // Called when the game starts or when spawned
 void ALumberMill::BeginPlay()
 {
 	Super::BeginPlay();
-	UE_LOG(LogTemp, Warning, TEXT("XDDDDD CYLINDER") );
+	IconWidget = NewObject<UIconWidget>();
+	IconWidget->Initialize();
+	if (IconWidget != nullptr)
+		UE_LOG(LogTemp, Warning, TEXT("IconWidget is not null - beginplay") );
 }
 
 // Called every frame
@@ -26,4 +35,3 @@ void ALumberMill::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 }
-
