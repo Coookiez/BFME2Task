2d1db80ebae3a5a668a92e57b1947574532a22e3
Coookiez <insanecookiez22@gmail.com>
2021-01-10 02:45:31 +0100

    Spawning Workers on icon click.

--- Source/BFME2Task/LumberMill.cpp
+++ Source/BFME2Task/LumberMill.cpp
@@ -2,10 +2,9 @@
 
 
 #include "LumberMill.h"
-#include "IconWidget.h"
-
 #include <iostream>
 
+#include "BFME2TaskCharacter.h"
 
 // Sets default values
 ALumberMill::ALumberMill()
@@ -14,21 +13,45 @@ ALumberMill::ALumberMill()
 	PrimaryActorTick.bCanEverTick = true;
 }
 
+void ALumberMill::SpawnWorker(UPrimitiveComponent*, FKey)
+{
+	FVector Location(0.0f, 0.0f, 265.588348f);
+	FRotator Rotation(0.0f, 0.0f, 0.0f);
+	FActorSpawnParameters SpawnInfo;
+	GetWorld()->SpawnActor<AWorker>(ObjectToSpawn, Location, Rotation, SpawnInfo);
+}
+
 // Called when the game starts or when spawned
 void ALumberMill::BeginPlay()
 {
 	Super::BeginPlay();
 	UIComponent = FindComponentByClass<UUIComponent>();
+
+	TArray<UActorComponent*> Meshes = GetComponentsByClass(UStaticMeshComponent::StaticClass());
+	for (auto x : Meshes)
+	{
+		if (x->GetName() == "Plane")
+		{
+			PlaneComponent = Cast<UStaticMeshComponent>(x);
+			break;
+		}
+	}
+	UE_LOG(LogTemp, Warning, TEXT("plane:  %s"), *PlaneComponent->GetName());
+
+	PlaneComponent->OnClicked.AddDynamic(this,  &ALumberMill::SpawnWorker);
+
 	if (UIComponent != nullptr)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Nie null") );
+		UE_LOG(LogTemp, Warning, TEXT("Nie null"));
 		UIComponent->AddUIToViewport();
 	}
+	
 }
+
+
 
 // Called every frame
 void ALumberMill::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
--- Source/BFME2Task/LumberMill.h
+++ Source/BFME2Task/LumberMill.h
@@ -6,23 +6,29 @@
 
 #include "IconWidget.h"
 #include "UIComponent.h"
+#include "Worker.h"
 #include "GameFramework/Actor.h"
 #include "LumberMill.generated.h"
+
 
 UCLASS()
 class BFME2TASK_API ALumberMill : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
 	// Sets default values for this actor's properties
 	ALumberMill();
+
+private:
+	UFUNCTION()
+	void SpawnWorker(UPrimitiveComponent* Component, FKey Key);
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
@@ -34,4 +40,10 @@ public:
 
 	UPROPERTY(VisibleAnywhere)
 	UUIComponent* UIComponent;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* PlaneComponent;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<AWorker> ObjectToSpawn;
 };
